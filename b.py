af301a220d77c2c036fcb7689403190c97db6b7b
eqxph <742738858@qq.com>
2026-02-18 13:01:32 +0800

    add function b

---
+++ b.py
@@ -0,0 +1,2 @@
+def funcb():
+    print('bbbb')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import b
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,5 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    b.funcb()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
